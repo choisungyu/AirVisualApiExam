f54fae4d0cf7d166c383f1c4e0b8b53b85825a25
Junsuk Oh <a811219@gmail.com>
2019-05-02 15:36:03 +0900

    기존 방식의 네비게이션

--- app/src/main/java/com/csg/airvisualapiexam/MainActivity.java
+++ app/src/main/java/com/csg/airvisualapiexam/MainActivity.java
@@ -11,33 +11,37 @@ import com.csg.airvisualapiexam.ui.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    AirVisualFragment airVisualFragment = new AirVisualFragment();
+    MapFragment mapFragment = new MapFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
-        AirVisualFragment airVisualFragment = new AirVisualFragment();
-        MapFragment mapFragment = new MapFragment();
 
         if (savedInstanceState == null) {
-
+            bottomNavigationView.setSelectedItemId(R.id.airvisual);
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, airVisualFragment)
-                    .commit();
-
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, mapFragment)
+                    .replace(R.id.container, airVisualFragment)
                     .commit();
         }
-
-
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
-//                    case R.id.
+                    case R.id.map:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, mapFragment)
+                                .commit();
+                        break;
+                    case R.id.airvisual:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, airVisualFragment)
+                                .commit();
+                        break;
                 }
                 return true;
             }
