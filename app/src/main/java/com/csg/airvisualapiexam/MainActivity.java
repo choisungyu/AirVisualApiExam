60366dd285d11815cd369b7e919578e04d17f3ed
Junsuk Oh <a811219@gmail.com>
2019-05-02 15:50:09 +0900

    새로운 방식의 네비게이션

--- app/src/main/java/com/csg/airvisualapiexam/MainActivity.java
+++ app/src/main/java/com/csg/airvisualapiexam/MainActivity.java
@@ -1,18 +1,14 @@
 package com.csg.airvisualapiexam;
 
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.MenuItem;
 
-import com.csg.airvisualapiexam.ui.AirVisualFragment;
-import com.csg.airvisualapiexam.ui.MapFragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-
-    AirVisualFragment airVisualFragment = new AirVisualFragment();
-    MapFragment mapFragment = new MapFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,32 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
 
-        if (savedInstanceState == null) {
-            bottomNavigationView.setSelectedItemId(R.id.airvisual);
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, airVisualFragment)
-                    .commit();
-        }
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.map:
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.container, mapFragment)
-                                .commit();
-                        break;
-                    case R.id.airvisual:
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.container, airVisualFragment)
-                                .commit();
-                        break;
-                }
-                return true;
-            }
-        });
-
-
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
